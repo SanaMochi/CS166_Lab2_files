fe6a9f6ddb174701c61a103b23b8c9d0ef9e9377
Sana Mohiuddin <smohi002@wch135-09.cs.ucr.edu>
2021-04-25 18:11:31 -0700

    debugged typos

--- CS166_Lab3.sql
+++ CS166_Lab3.sql
@@ -13,19 +13,22 @@ VALUES (862342342, 'Lauren Sinclair', 22, 1, 'Forensics');
 INSERT INTO Professor(ssn, name, age, rank, specialty)
 VALUES (862324567, 'Kieran White', 24, 2, 'Contemporary Art');
 
-DROP TABLE IF EXISTS Project CASCADE;
-CREATE TABLE Project (pno NUMERIC (9, 0),
+DROP TABLE IF EXISTS Project_Manages CASCADE;
+CREATE TABLE Project_Manages (pno NUMERIC (9, 0),
 			ssn NUMERIC (9, 0), --Prof ssn
 			sponsor CHAR(20),
 			start_date DATE,
 			end_date DATE,
 			budget INTEGER,
-			PRIMARY KEY(pno)), 
+			PRIMARY KEY(pno), 
 			FOREIGN KEY (ssn) REFERENCES Professor(ssn) -- Prof manages
 			ON DELETE CASCADE); -- participation constraint
 
-DROP TABLE IF EXISTS Dept CASCADE;
-CREATE TABLE Dept (dno NUMERIC (9, 0),
+INSERT INTO Project_Manages(pno, ssn, sponsor, start_date, end_date, budget)
+VALUES (000000001, 862342342, 'APD', '1890-12-20', '1892-03-12', 800);
+
+DROP TABLE IF EXISTS Dept_Runs CASCADE;
+CREATE TABLE Dept_Runs (dno NUMERIC (9, 0),
 			ssn NUMERIC (9, 0),
 			dname CHAR(30),
 			office CHAR(20),
@@ -33,13 +36,16 @@ CREATE TABLE Dept (dno NUMERIC (9, 0),
 			FOREIGN KEY (ssn) REFERENCES Professor(ssn) -- Prof runs
 			ON DELETE CASCADE); -- Participation constraint
 
-DROP TABLE IF EXISTS Graduate CASCADE;
-CREATE TABLE Graduate (gssn NUMERIC (9, 0),
+INSERT INTO Dept_Runs(dno, ssn, dname, office)
+VALUES (100000000, 862342342, 'phantom scythe', 'homicide');
+
+DROP TABLE IF EXISTS Graduate_Major CASCADE;
+CREATE TABLE Graduate_Major (gssn NUMERIC (9, 0),
 			dno NUMERIC (9, 0),	-- Dept ID
 			gname CHAR(30) NOT NULL,
 			age INTEGER,
 			deg_pg CHAR(20),
-			PRIMARY KEY(gssn)),	--TA said unecessary but idk kinda need it
+			PRIMARY KEY(gssn),
 			FOREIGN KEY (dno) REFERENCES Dept(dno)	-- Major
 			ON DELETE CASCADE);	-- participation consraint
 
@@ -59,7 +65,7 @@ CREATE TABLE work_dept (ssn NUMERIC (9, 0),
 			FOREIGN KEY (dno) REFERENCES Dept(dno));
 
 DROP TABLE IF EXISTS work_proj_supervise_manage_advise CASCADE;	--aggregate
-CREATE TABLE work_proj (ssn NUMERIC (9, 0),
+CREATE TABLE work_proj_supervise_manage_advise (ssn NUMERIC (9, 0),
 			gssn NUMERIC (9, 0),
 			pno NUMERIC (9, 0),
 			since CHAR(10),
@@ -79,18 +85,18 @@ CREATE TABLE Advise (advisor_ssn NUMERIC (9, 0),
 
 --Notown Database--
 
-OP TABLE IF EXISTS Musician CASCADE;
+DROP TABLE IF EXISTS Musician CASCADE;
 CREATE TABLE Musician (ssn NUMERIC (9, 0),
 			name CHAR(30),
 			PRIMARY KEY(ssn));
 
 DROP TABLE IF EXISTS Album_Producer CASCADE;
-CREATE TABLE Album (albumid NUMERIC (9, 0),
+CREATE TABLE Album_Producer (albumid NUMERIC (9, 0),
 			ssn NUMERIC (9, 0),	-- Musician ssn
 			copyright_date DATE,
 			speed INTEGER,
 			title CHAR(20),
-			PRIMARY KEY(albumid)
+			PRIMARY KEY(albumid),
 			FOREIGN KEY(ssn) REFERENCES Musician(ssn));	--Producer reation
 
 DROP TABLE IF EXISTS Instrument CASCADE;
@@ -104,13 +110,13 @@ CREATE TABLE Place (address CHAR(30),
 			PRIMARY KEY(address));
 
 DROP TABLE IF EXISTS Telephone_Home CASCADE;
-CREATE TABLE Telephone (phone_no CHAR(11),
+CREATE TABLE Telephone_Home (phone_no CHAR(11),
 			address CHAR(30),
 			PRIMARY KEY(phone_no),
 			FOREIGN KEY(address) REFERENCES Place(address));
 
 DROP TABLE IF EXISTS Songs_Appears CASCADE;
-CREATE TABLE Songs (songid NUMERIC (9, 0),
+CREATE TABLE Songs_Appears (songid NUMERIC (9, 0),
 			albumid NUMERIC (9, 0),	-- Album id
 			title CHAR(20),
 			author CHAR(20),
@@ -138,7 +144,7 @@ CREATE TABLE Lives (ssn NUMERIC (9, 0),
 			PRIMARY KEY(ssn),
 			FOREIGN KEY(ssn) REFERENCES Musician(ssn),
 			FOREIGN KEY(address) REFERENCES Place(address),
-			FOREIGN KEY(phone_no) REFERENCES Telephone_Home(phone_no),);
+			FOREIGN KEY(phone_no) REFERENCES Telephone_Home(phone_no));
 
 
 
