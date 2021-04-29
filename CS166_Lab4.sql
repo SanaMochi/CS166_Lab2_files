09ae14fd4e64e3622ae9d5a91f41d514df985edd
Sana Mohiuddin <smohi002@ucr.edu>
2021-04-29 16:46:50 -0700

    Finished sql conversion

--- CS166_Lab4.sql
+++ CS166_Lab4.sql
@@ -20,7 +20,7 @@ CREATE TABLE Books (docid NUMERIC (9, 0),
 			PRIMARY KEY(docid));
 
 INSERT INTO Books (docid, title, publisher, year)
-VALUES (225200414, 'No Longer Human', 'Penguin', 1948);
+VALUES (225200414, 'No Longer Human', 'McGraw-Hill', 1948);
 INSERT INTO Books (docid, title, publisher, year)
 VALUES (225200928, 'Beast Under the Moonllight', 'Penguin', 1956);
 
@@ -61,4 +61,63 @@ CREATE TABLE describes (docid NUMERIC (9, 0),
 			PRIMARY KEY (keyword),
 			FOREIGN KEY (docid) REFERENCES Books(docid));
 
+
+--1--
+SELECT B.year, B.title
+FROM Books B
+
+--2--
+SELECT *
+FROM Students S
+WHERE S.major='CS'
+
+--3--
+SELECT *
+FROM Books B, Students S
+
+--4--
+SELECT B.publisher, B.bdate
+FROM Books B
+WHERE B.publisher='McGraw-Hill' AND B.bdate<1990
+
+--5--
+SELECT A.aname
+FROM Authors A
+WHERE A.Address LIKE '%Davis%'
+
+--6--
+SELECT S.stname
+FROM Students S
+WHERE S.age>30 AND S.major!='CS'
+
+--7--
+ALTER TABLE Authors
+CHANGE COLUMN aname TO name
+
+--8--
+SELECT S.stname
+FROM Students S, borrows B
+WHERE S.stid = B.stid AND S.major='CS'
+
+--9--
+SELECT B.title
+FROM Books B, has-written H
+WHERE B.docid = H.docid AND B.name='Jones'
+
+--10--
+SELECT B.title
+FROM Books B, has-written, describes D
+WHERE B.docid = H.docid AND B.docid = D.docid AND B.name-'Jones'AND D.keyword!='database'
+
+--11--
+SELECT S.stname
+FROM Students S
+WHERE S.age = (SELECT MIN(S2.age))
+		FROM Students S2
+
+--12--
+SELECT B.title
+FROM Books B
+WHERE B.year = (SELECT MIN(B2.year)
+		FROM Books B2)
 
