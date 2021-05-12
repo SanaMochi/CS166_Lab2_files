340504e75dab479a8cf78cff0be6159e7f095913
Sana Mohiuddin <smohi002@wch135-09.cs.ucr.edu>
2021-05-12 09:56:46 -0700

    Started part 1

--- lab6/lab5/queries.sql
+++ lab6/lab5/queries.sql
@@ -1,26 +1,26 @@
--- (1) pid of parts with costs lower than 10
-SELECT C.pid
-FROM catalog C
-WHERE C.cost < 10;
-
--- (2) name of parts with costs lower than 10
-SELECT P.pname
-FROM catalog C, parts P
-WHERE C.pid = P.pid AND C.cost < 10;
-
--- (3) address of suppliers who supply part "Fire Hydrant Cap"
-SELECT S.address
+-- (1) total number of parts supplied by each supplier
+SELECT COUNT(*)
 FROM catalog C, parts P, suppliers S
-WHERE C.pid = P.pid AND C.sid = S.sid AND P.pname = 'Fire Hydrant Cap';
+WHERE C.pid = P.pid AND C.sid = S.sid
+GROUP BY S.sid;
 
--- (4) name of suppliers who supply green parts
-SELECT S.sname
+-- (2) total number of parts supplied by each supplier who supplies at least 3 parts
+SELECT COUNT(*)
 FROM catalog C, parts P, suppliers S
-WHERE C.pid = P.pid AND C.sid = S.sid AND P.color = 'Green';
+WHERE C.pid = P.pid AND C.sid = S.sid AND 2 < (SELECT COUNT(*) 
+					       FROM catalog C, parts P
+					       WHERE C.pid = P.pid AND C.sid = S.sid)
+GROUP BY S.sid;
 
--- (5) for each supplier, list supplier's name along with all parts' name that it supply
-SELECT DISTINCT S.sname, P.pname
-FROM catalog C, parts P, suppliers S
-WHERE C.pid = P.pid AND C.sid = S.sid;
+-- (3) For every supplier that supplies only green parts, print the name of the supplier 
+--	& the total number of parts that he supplies
+--SELECT S.address
+--FROM catalog C, parts P, suppliers S
+--WHERE C.pid = P.pid AND C.sid = S.sid AND P.pname = 'Fire Hydrant Cap';
 
+-- (4) for ever supplier that supplies green parts & red parts print the name of the supplier
+--	& the price of the most expensive part he supplies
+--SELECT S.sname
+--FROM catalog C, parts P, suppliers S
+--WHERE C.pid = P.pid AND C.sid = S.sid AND P.color = 'Green';
 
