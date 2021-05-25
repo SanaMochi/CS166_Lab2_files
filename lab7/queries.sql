d681cf9d67d8647fd72fedde592bc4737b2e2b54
Sana Mohiuddin <smohi002@ucr.edu>
2021-05-25 10:15:03 -0700

    Completed part 1

--- lab7/queries.sql
+++ lab7/queries.sql
@@ -1,18 +1,31 @@
 -- (1) Count how many parts in NYC have more than 70 parts on_hand
-
+SELECT COUNT(N.on_hand)
+FROM part_nyc N
+WHERE N.on_hand > 70;
 
 -- (2) Count how many total parts on hand in both NYC and SFO are Red
-
+SELECT (SUM(N.on_hand) + SUM(S.on_hand))
+FROM part_nyc N, part_sfc S
+WHERE N.color = 'Red' OR S.color = 'Red';
 
 -- (3) List all the suppliers that have more total on_hand parts in NYC than they do in SFO
+SELECT N.supplier
+FROM part_nyc N, part_sfo S
+WHERE N.part_number = S.part_number AND 
+	(SELECT SUM(N2.on_hand) FROM part_nyc N2 WHERE N.supplier = N2.supplier) > 
+	(SELECT SUM(S2.on_hand) FROM part_sfo S2 WHERE S.supplier = S2.supplier);
 
-
--- (4) List al suppliers that supply parts in NYC that aren't supplied by anyone in SFO
-
+-- (4) List all suppliers that supply parts in NYC that aren't supplied by anyone in SFO
+SELECT N.supplier
+FROM part_nyc N
+WHERE NOT EXISTS (SELECT S.supplier FROM part_sfo S WHERE N.part_number = S.part_number);
 
 -- (5) Update all of the NYC on_hand values - 10
-
+UPDATE part_nyc N
+SET N.on_hand = N.on_hand - 10;
 
 -- (6) Delete all parts from NYC which have less than 30 parts on_hand
-
+DELETE *
+FROM part_nyc N
+WHERE N.on_hand < 30;
 
