a340c0f64746df57e9370c4a4ea916dd6a17b7dc
Sana Mohiuddin <smohi002@wch135-09.cs.ucr.edu>
2021-05-27 16:32:38 -0700

    Added indexes

--- lab7/create_indexes.sql
+++ lab7/create_indexes.sql
@@ -1,3 +1,28 @@
--- Indexes of the queries in queries.sql
+-- Indexes of the tables
+DROP INDEX IF EXISTS nyc;
+DROP TABLE IF EXISTS sfo;
+DROP TABLE IF EXISTS sup;
+DROP TABLE IF EXISTS col;
+
+CREATE INDEX nyc
+ON part_nyc
+USING BTREE
+(on_hand);
+
+CREATE INDEX sfo
+ON part_sfo
+USING BTREE
+(on_hand);
+
+CREATE INDEX sup
+ON supplier
+USING BTREE
+(supplier_id);
+
+CREATE INDEX COL
+ON color
+USING BTREE
+(color_id);
+
 
 
