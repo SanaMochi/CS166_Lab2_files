5c78e5839a7200afd76a671d32f5eff3a02d7446
Sana Mohiuddin <smohi002@wch135-09.cs.ucr.edu>
2021-05-27 15:25:17 -0700

    fixed stuff

--- lab7/queries.sql
+++ lab7/queries.sql
@@ -4,29 +4,32 @@ FROM part_nyc N
 WHERE N.on_hand > 70;
 
 -- (2) Count how many total parts on hand in both NYC and SFO are Red
-SELECT (SUM(N.on_hand) + SUM(S.on_hand))
-FROM part_nyc N, part_sfo S
-WHERE N.color = (SELECT color_id FROM color C WHERE C.color_name = 'Red') OR 
-      S.color = (SELECT color_id FROM color C WHERE C.color_name = 'Red');
+--SELECT SUM(N.on_hand)
+--FROM part_nyc N, part_sfo S
+--WHERE N.color = (SELECT color_id FROM color C WHERE C.color_name = 'Red')  
+--UNION
+--SELECT SUM(S.on_hand) 
+--FROM part_sfo 
+--WHERE S.color = (SELECT color_id FROM color C WHERE C.color_name = 'Red');
 
 -- (3) List all the suppliers that have more total on_hand parts in NYC than they do in SFO
-SELECT N.supplier
-FROM part_nyc N, part_sfo S
-WHERE N.part_number = S.part_number AND 
-	(SELECT SUM(N2.on_hand) FROM part_nyc N2 WHERE N.supplier = N2.supplier) > 
-	(SELECT SUM(S2.on_hand) FROM part_sfo S2 WHERE S.supplier = S2.supplier);
+--SELECT N.supplier
+--FROM part_nyc N, part_sfo S
+--WHERE N.part_number = S.part_number AND 
+--	(SELECT SUM(N2.on_hand) FROM part_nyc N2 WHERE N.supplier = N2.supplier) > 
+--	(SELECT SUM(S2.on_hand) FROM part_sfo S2 WHERE S.supplier = S2.supplier);
 
 -- (4) List all suppliers that supply parts in NYC that aren't supplied by anyone in SFO
 SELECT N.supplier
 FROM part_nyc N
-WHERE NOT EXISTS (SELECT S.supplier FROM part_sfo S WHERE N.part_number = S.part_number);
+WHERE N.supplier NOT IN (SELECT S.supplier FROM part_sfo S WHERE N.part_number = S.part_number);
 
 -- (5) Update all of the NYC on_hand values - 10
-UPDATE part_nyc N
-SET N.on_hand = N.on_hand - 10;
+UPDATE part_nyc
+SET on_hand = on_hand - 10;
 
 -- (6) Delete all parts from NYC which have less than 30 parts on_hand
-DELETE *
-FROM part_nyc N
-WHERE N.on_hand < 30;
+DELETE
+FROM part_nyc
+WHERE on_hand < 30;
 
